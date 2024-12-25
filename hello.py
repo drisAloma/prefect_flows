893334d750ab86dace41ede25b1638358c25e35b
Lawal Idris <64994788+drisAloma@users.noreply.github.com>
2024-12-25 22:23:21 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def hello_world(name: str = "world", goodbye: bool = False):
     if goodbye:
         print(f"Goodbye {name}!")
 
-print(hello_world())
+print(hello_world(goodbye=True))
 
 # if __name__ == "__main__":
 #     hello_world()
