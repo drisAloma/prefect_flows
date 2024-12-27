238315912443404c515eec3117fa63adbc9abe70
Lawal Idris <64994788+drisAloma@users.noreply.github.com>
2024-12-27 11:52:46 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 from prefect import flow
 
-
 @flow(log_prints=True)
 def hello_world(name: str = "world", goodbye: bool = True):
     print(f"Hello {name} from Prefect! 🤗")
 
     if goodbye:
         print(f"Goodbye {name}!")
-
-# print(hello_world())
 
 # if __name__ == "__main__":
 #     hello_world()
